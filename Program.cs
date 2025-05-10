f913141a81b5fc634a599bac9c09bb05fd9e183e
Lynnerv <aythor_briceno@usmp.pe>
2025-05-09 19:22:17 -0500

    base de datos

--- Program.cs
+++ Program.cs
@@ -1,15 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using APIV22.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Conexión a PostgreSQL
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -22,6 +26,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Zapatilla}/{action=Index}/{id?}"); // Cambié Home por Zapatilla si quieres que sea la vista principal
 
 app.Run();
